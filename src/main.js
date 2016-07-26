3eca4c4c47271dc7688071c14b807d717d9cee15
Kristjan Gannon <gannonk08@gmail.com>
2016-07-26 11:49:49 -0600

    JS with movie selector

--- src/main.js
+++ src/main.js
@@ -1,16 +1,29 @@
 $(function() {
+
   console.log("sanity check");
-  $('#searchBar').on('submit', function(event){
+  $('#searchButton').on('click', function(event){
     event.preventDefault();
     var movieTitle = $('#searchBar').val();
+    $('titleHolder').append(movieTitle)
     var url = 'http://www.omdbapi.com/?t=' + movieTitle
+    console.log(url);
+    // var url = 'http://www.omdbapi.com/?t=jaws'
     $.ajax({
       method: 'GET',
       dataType: 'json',
       url: url
+    }).done(function(results){
+      var genre = results.Genre
+      console.log(genre);
+      var posterUrl = results.Poster
+      $("#movieImage").attr('src', posterUrl);
+      var seperateGenres = genre.split(',');
+      for (var i = 0; i < seperateGenres.length; i++) {
+        $('#selectBar').append('<option>'+ seperateGenres[i] + '</option>')
+      }
+
+
+
     })
-
-
-  })
-
-}());
+})
+});
